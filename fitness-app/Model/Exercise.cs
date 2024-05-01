4b03f3a0b4eb59f2a00640c95b7ebd9d58ad164d
Domantas Bacius <87576175+TisIDom@users.noreply.github.com>
2024-05-01 17:59:00 +0300

    Added exercise models

---
+++ fitness-app/Model/Exercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessApp.Model;
+
+public class Exercise
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public List<Muscle> Muscles { get; set; }
+    [Required]
+    public string Type { get; set; }
+}
---
+++ fitness-app/Model/Set.cs
@@ -0,0 +1,29 @@
+﻿using FitnessApp.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessApp.Model;
+
+public class Set
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public int Reps { get; set; }
+    [Required]
+    public int Weight { get; set; }
+    [Required]
+    public string Type { get; set; }
+    [Required]
+    public int RestTime { get; set; }
+    [Required]
+    public int Order { get; set; }
+    [Required]
+    public int WorkoutId { get; set; }
+    public Workout Workout { get; set; }
+    [Required]
+    public int ExerciseId { get; set; }
+    public Exercise Exercise { get; set; }
+    public string? Notes { get; set; }
+}
