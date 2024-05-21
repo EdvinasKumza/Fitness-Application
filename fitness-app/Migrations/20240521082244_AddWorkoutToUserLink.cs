4fa86dbd34c6e3bf1e3129d0b9af290a0887a8a0
Domantas Bacius <87576175+TisIDom@users.noreply.github.com>
2024-05-21 11:28:50 +0300

    created workout to user link and migration

---
+++ fitness-app/Migrations/20240521082244_AddWorkoutToUserLink.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkoutToUserLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Workouts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_UserId",
+                table: "Workouts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Workouts_Users_UserId",
+                table: "Workouts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Workouts_Users_UserId",
+                table: "Workouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Workouts_UserId",
+                table: "Workouts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Workouts");
+        }
+    }
+}
